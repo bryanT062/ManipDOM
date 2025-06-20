83b40c5330212068962b66852699ff6ce58ad3f5
bryanT062 <bryantate899@gmail.com>
2025-06-20 16:38:16 +0200

    debut

---
+++ carroussel.js
@@ -0,0 +1,66 @@
+const section = document.querySelector("section");
+section.innerHTML = `
+<div style="display: flex; align-items: center; gap: 20px;">
+<button type="button" id="left"> < </button>
+<img id="carousel-img" src="" alt="" width="600" object-fit="cover">
+<button type="button" id="right"> > </button>
+</div>`;
+
+const imagePaths = [
+    "./carroussel/images/pexels-edwin-soby-2150975761-31496663.jpg",
+  "./carroussel/images/pexels-hub-jacqu-750015482-29032250.jpg",
+  "./carroussel/images/pexels-marinca-liviu-940909768-20064489.jpg",
+];
+
+section.style.textAlign ="center";
+let changer = 0;
+
+function changerImage() {
+  const img = document.getElementById("carousel-img");
+  img.style.opacity = 0;
+  setTimeout(() => {
+    img.src = imagePaths[changer];
+    img.style.opacity = 1;
+    
+  }, 200); 
+}
+
+function carroussel(direction) {
+  if (direction === 'left') {
+    changer = (changer - 1 + imagePaths.length) % imagePaths.length;
+  } else if (direction === 'right') {
+    changer = (changer + 1) % imagePaths.length;
+  }
+  changerImage();
+}
+
+document.getElementById("left").addEventListener("click", () => carroussel('left'));
+document.getElementById("right").addEventListener("click", () => carroussel('right'));
+
+changerImage();
+
+section.style.marginLeft = "33%";
+
+
+const app = document.getElementById("div");
+
+
+
+app.innerHTML = `
+<div style=" align-items: center; gap: 20px;">
+  <input type="text" id="input" placeholder="Votre produit" textalign:center;>
+  <button id="add">Ajouter</button>
+  <ul id="list"></ul>
+</div>
+`;
+
+const add = document.getElementById("add");
+
+add.addEventListener("click", function() {
+  const input = document.getElementById("input");
+  const list = document.getElementById("list");
+  const li = document.createElement("li");
+  li.textContent = input.value;
+  list.appendChild(li);
+  input.value = "";
+});
